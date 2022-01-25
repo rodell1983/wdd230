cd682d55f83ae7afac803768f41aa6d5788de155
rodell1983 <ode21001@byui.edu>
2022-01-25 13:46:25 -0800

    test

---
+++ chamber/js/script.js
@@ -0,0 +1,22 @@
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector("date");
+
+// derive the current date using a date object
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldateUK}</em>`;
