4834e2327b3c3bef3fe8f0483623b785f4b1cff9
rodell1983 <ode21001@byui.edu>
2022-01-30 18:13:49 -0800

    update

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,4 +1,11 @@
 
+let date = new Date(document.lastModified);
+let dateString = `Last Updated: ${date.toLocaleString()}`;
+
+document.getElementById("current-year").textContent = date.getFullYear();
+document.getElementById("current-date").textContent = dateString;
+
+
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
