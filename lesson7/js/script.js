82abb5df80c93accc4f038a8db13f2c36385fd51
rodell1983 <ode21001@byui.edu>
2022-02-14 20:05:37 -0800

    first draft

---
+++ lesson7/js/script.js
@@ -0,0 +1,34 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: '0px 0px -300px 0px'
+}
+
+const loadImages = (img) => {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = () => {
+        img.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                loadImages(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    imagesToLoad.forEach((image) => {
+        observer.observe(image);
+    });
+} else {
+    imagesToLoad.forEach((image) => {
+        loadImages(image);
+    });
+}
+
+
