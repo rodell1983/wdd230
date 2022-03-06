f3dd35393c2d98b03b3ff4491b42bad8a23facb2
rodell1983 <ode21001@byui.edu>
2022-03-06 09:12:35 -0800

    update directory

--- chamber/js/businesses.js
+++ chamber/js/businesses.js
@@ -8,32 +8,40 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const businesses = jsonObject["business"];
     businesses.forEach(displayBusiness);
+    loadSettings();
   });
 
 function displayBusiness(business) {
   // Create elements to add to the document
   let card = document.createElement("section");
   let image = document.createElement("img");
-  let h2 = document.createElement("h2");
+  let name = document.createElement("p");
   let address = document.createElement("p");
   let phone = document.createElement("p");
+  let website = document.createElement("a");
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
   image.setAttribute("src", business.logourl);
   image.setAttribute("loading", "lazy");
 
   // Change the textContent property of the h2 element to contain the prophet's full name
-  h2.textContent = `${business.name}`;
+  name.textContent = business.name;
 
   address.textContent = business.address;
 
   phone.textContent = business.phone;
 
+  let linkText = document.createTextNode(business.website);
+  website.appendChild(linkText);
+  website.title = business.name;
+  website.href = business.website;
+
   // Add/append the section(card) with the h2 element
   card.appendChild(image);
-  card.appendChild(h2);
+  card.appendChild(name);
   card.appendChild(address);
   card.appendChild(phone);
+  card.appendChild(website);
 
   card.classList.add("card");
 
@@ -41,36 +49,55 @@ function displayBusiness(business) {
   document.querySelector("div.business-cards").appendChild(card);
 }
 
-document
-  .querySelector(".list-view-button")
-  .addEventListener("click", function () {
-    const cards = document.querySelectorAll(".card");
+function listView() {
+  const cards = document.querySelectorAll(".card");
 
-    for (const c of cards) {
-      c.classList.add("card-list");
-      c.classList.remove("card-grid");
-      c.classList.remove("block");
-      c.classList.remove("shadow");
+  for (const c of cards) {
+    c.classList.add("card-list");
+    c.classList.remove("card-grid");
+    c.classList.remove("block");
+    c.classList.remove("shadow");
+  }
+
+  let frame = document.querySelector(".business-cards");
+  frame.classList.remove("grid");
+  frame.classList.add("list");
+
+  localStorage.setItem("view", "list");
+}
+
+function gridView() {
+  const cards = document.querySelectorAll(".card");
+
+  for (const c of cards) {
+    c.classList.remove("card-list");
+    c.classList.add("card-grid");
+    c.classList.add("block");
+    c.classList.add("shadow");
+  }
+
+  let frame = document.querySelector(".business-cards");
+  frame.classList.add("grid");
+  frame.classList.remove("list");
+
+  localStorage.setItem("view", "grid");
+}
+
+function loadSettings() {
+  //Load Local Storage
+  const v = localStorage.getItem("view");
+
+  // Display welcome message based on last visit day
+  if (v === null) {
+    listView();
+  } else {
+    if (v === "list") {
+      listView();
+    } else {
+      gridView();
     }
+  }
+}
 
-    let frame = document.querySelector(".business-cards");
-    frame.classList.remove("grid");
-    frame.classList.add("list");
-  });
-
-document
-  .querySelector(".grid-view-button")
-  .addEventListener("click", function () {
-    const cards = document.querySelectorAll(".card");
-
-    for (const c of cards) {
-      c.classList.remove("card-list");
-      c.classList.add("card-grid");
-      c.classList.add("block");
-      c.classList.add("shadow");
-    }
-
-    let frame = document.querySelector(".business-cards");
-    frame.classList.add("grid");
-    frame.classList.remove("list");
-  });
+document.querySelector(".list-view-button").addEventListener("click", listView);
+document.querySelector(".grid-view-button").addEventListener("click", gridView);
