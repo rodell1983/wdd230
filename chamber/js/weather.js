5d9034e6569a5ac483181ba81accd80a5d66d4ca
rodell1983 <ode21001@byui.edu>
2022-03-12 14:44:08 -0800

    update weather

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,34 +1,54 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5506956&appid=ca6d3a3994d6211dbf7bfd04bd2903b4&lang=en&units=imperial"
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=5506956&appid=ca6d3a3994d6211dbf7bfd04bd2903b4&lang=en&units=imperial";
 
 fetch(apiURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-
     // Set Variables
     let temp = jsonObject.main.temp;
     let windSpeed = jsonObject.wind.speed;
-    let chill = windChill(
-      temp,
-      windSpeed);
-    
-    let weatherDesc = jsonObject.weather[0].main.toLowerCase();
+    let chill = windChill(temp, windSpeed);
 
-    //Weather Values
-    document.getElementById("weather-temp").textContent = Math.round(temp);
-    document.getElementById("weather-windspeed").textContent = Math.round(windSpeed);
-    document.getElementById("weather-windchill").textContent = chill;
+    let weatherDesc = jsonObject.weather[0].main.toLowerCase();
+    let weatherDescText = jsonObject.weather[0].description;
+
+    let sky = document.createElement("div");
+    let weatherImg = document.createElement("img");
+    let weatherTemp = document.createElement("span");
+    let weatherDT = document.createElement("span");
+    let weatherSpeedChill = document.createElement("dl");
+
+    weatherTemp.innerHTML = `${Math.round(temp)}<sup>°F</sup>`;
+    weatherTemp.classList.add("temp")
+
+    weatherDT.innerHTML = weatherDescText;
+
+    let mph = "mph";
+    if (chill === "N/A"){
+      mph = "";
+    }
+    weatherSpeedChill.innerHTML = `<dt>Wind Speed:</dt><dd>${Math.round(
+      windSpeed
+    )} mph</dd><dt>Wind Chill:</dt><dd>${chill} ${mph}</dd>`;
 
     //Weather Icon Image
-    imgEl = document.getElementById("weather-img");
-    imgEl.src = `images/weather/${weatherDesc}.png`;
-    imgEl.alt = weatherDesc;
-    imgEl.addEventListener("error", function(event) {
+    weatherImg.src = `images/weather/${weatherDesc}.png`;
+    weatherImg.alt = weatherDesc;
+    weatherImg.addEventListener("error", function (event) {
       event.target.src = `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
-      event.onerror = null
+      event.onerror = null;
     });
 
+    sky.appendChild(weatherImg);
+    sky.appendChild(weatherTemp);
+
+    weatherBlock = document.querySelector(".weather");
+
+    weatherBlock.appendChild(sky);
+    weatherBlock.appendChild(weatherDT);
+    weatherBlock.appendChild(weatherSpeedChill);
   });
 
 function windChill(t, s) {
