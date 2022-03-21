31bde801e2a81d0fd427fa304d2d76ce61078fdf
rodell1983 <ode21001@byui.edu>
2022-03-20 18:04:14 -0700

    rename

--- Final/siteplan/js/script.js
+++
@@ -1,5 +0,0 @@
-let date = new Date(document.lastModified);
-let dateString = `Last Updated: ${date.toLocaleString()}`;
-
-document.getElementById("current-year").textContent = date.getFullYear();
-document.getElementById("current-date").textContent = dateString;
