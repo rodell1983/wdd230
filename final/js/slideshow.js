928ca3b77a9946e2355553506e9f6535f4e79169
rodell1983 <ode21001@byui.edu>
2022-03-28 12:23:19 -0700

    json

--- final/js/slideshow.js
+++ final/js/slideshow.js
@@ -18,7 +18,7 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -28,9 +28,6 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+
 }
