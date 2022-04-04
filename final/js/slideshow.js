250089d896c827a0b6a6bc6c6c2c22ca0cff636b
rodell1983 <ode21001@byui.edu>
2022-04-04 11:33:49 -0700

    edit

--- final/js/slideshow.js
+++ final/js/slideshow.js
@@ -1,3 +1,11 @@
+let imgFiles = [
+  "cedar_city_utah_temple_med.webp",
+  "las_vegas_temple_med.webp",
+  "salt_lake_temple_med.webp",
+  "toronto-temple_flowers_med.webp",
+  "los_angeles_temple_med.webp"
+];
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -30,7 +38,29 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
+function buildSlide(path){
+  let slides = document.createElement("div");
+  slides.classList.add("slides");
+  let slidePic = document.createElement("picture");
+  let slideImg = document.createElement("img");
+  slideImg.setAttribute("src", path);
+  //slideImg.setAttribute("loading", "lazy");
+
+  slidePic.append(slideImg);
+  slides.appendChild(slidePic);
+
+  document.querySelector("div.slide-show").prepend(slides);
+}
+
 // Begin code
-let slideIndex = getRandomInt(4);
+let slideIndex = 0;
+
+imgFiles = imgFiles.sort(() => Math.random() - 0.5);
+
+for (let i = 0; i < 3; i++) {
+  buildSlide(`images/temples/${imgFiles[i]}`);
+}
+document.querySelector("div.default-slide").remove();
+
 showSlides(slideIndex);
 autoSlides();
